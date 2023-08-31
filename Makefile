b87e0ded047d30bbecb85bd3da4cb05f66a6c90c
Scott Cosentino <scottc130@gmail.com>
2023-08-31 13:19:16 -0400

    Update for tutorial 4

--- Makefile
+++ Makefile
@@ -3,35 +3,31 @@ ASM=nasm
 SRC_DIR=src
 BUILD_DIR=build
 
-.BUILDALL: all floppy_image kernel bootlader clean
-
+all: clean floppy_image bootloader kernel
 #
 # Floppy Image
 #
-floppy_image: $(BUILD_DIR)/main.img
 
+floppy_image: $(BUILD_DIR)/main.img
 $(BUILD_DIR)/main.img: bootloader kernel
 	dd if=/dev/zero of=$(BUILD_DIR)/main.img bs=512 count=2880
-	mkfs.fat -F 12 -n "JAZZ" $(BUILD_DIR)/main.img
+	mkfs.fat -F 12 -n "JAZZOS" $(BUILD_DIR)/main.img
 	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main.img conv=notrunc
 	mcopy -i $(BUILD_DIR)/main.img $(BUILD_DIR)/kernel.bin "::kernel.bin"
 
 #
 # Bootloader
 #
-
 bootloader: $(BUILD_DIR)/bootloader.bin
-
 $(BUILD_DIR)/bootloader.bin:
-	$(ASM) $(SRC_DIR)/bootloader/boot.asm -f bin -o $(BUILD_DIR)/bootloader.bin
+	$(ASM) $(SRC_DIR)/bootloader/boot.asm -f bin -o $(BUILD_DIR)/bootloader.bin    
 
 #
 # Kernel
 #
 kernel: $(BUILD_DIR)/kernel.bin
-
 $(BUILD_DIR)/kernel.bin:
 	$(ASM) $(SRC_DIR)/kernel/main.asm -f bin -o $(BUILD_DIR)/kernel.bin
 
 clean:
-	rm -rf $(BUILD_DIR)/*
+	rm -rf build/*
--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,30 +1,29 @@
 ORG 0x7C00
 BITS 16
 
-jmp short main
-nop
+JMP SHORT main
+NOP
 
-bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
-bdb_bytes_per_sector:       dw 512
-bdb_sectors_per_cluster:    db 1
-bdb_reserved_sectors:       dw 1
-bdb_fat_count:              db 2
-bdb_dir_entries_count:      dw 0E0h
-bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44MB
-bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy disk
-bdb_sectors_per_fat:        dw 9                    ; 9 sectors/fat
-bdb_sectors_per_track:      dw 18
-bdb_heads:                  dw 2
-bdb_hidden_sectors:         dd 0
-bdb_large_sector_count:     dd 0
+bdb_oem:        DB      'MSWIN4.1'
+bdb_bytes_per_sector:    DW  512
+bdb_sectors_per_cluster: DB  1
+bdb_reserved_sectors:    DW  1
+bdb_fat_count:          DB  2
+bdb_dir_entries_count:  DW  0E0h
+bdb_total_sectors:       DW  2880
+bdb_media_descriptor_type: DB 0F0h
+bdb_sectors_per_fat:     DW  9
+bdb_sectors_per_track:   DW  18
+bdb_heads:              DW  2
+bdb_hidden_sectors:     DD  0
+bdb_large_sector_count: DD  0
 
-; extended boot record
-ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
-                            db 0                    ; reserved
-ebr_signature:              db 29h
-ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
-ebr_volume_label:           db '  JAZZ OS  '        ; 11 bytes, padded with spaces
-ebr_system_id:              db 'FAT12   '           ; 8 bytes  '
+ebr_drive_number:       DB  0
+                        DB  0
+ebr_signature:           DB  29h
+ebr_volume_id:          DB  12h,34h,56h,78h
+ebr_volume_label:       DB 'JAZZ OS    '
+ebr_system_id:          DB  'FAT12   '
 
 main:
     MOV ax,0
@@ -33,12 +32,109 @@ main:
     MOV ss,ax
 
     MOV sp,0x7C00
+
+    mov [ebr_drive_number], dl
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+    
     MOV si,os_boot_msg
     CALL print
     HLT
 
 halt:
     JMP halt
+
+floppy_error:
+    MOV si, read_failure
+    call print
+    hlt
+
+;disk routines
+;cx [bits 0-5]: sector number
+;cx [bits 6-15]: cylinder
+;dh: head
+lba_to_chs:
+    push ax
+    push dx
+    
+    XOR dx,dx
+    div word [bdb_sectors_per_track]
+
+    inc dx
+    mov cx, dx
+
+    xor dx,dx
+    div word [bdb_heads]
+
+    mov dh,dl
+    mov ch,al
+    shl ah,6
+    or cl,ah
+
+    pop ax
+    mov dl,al
+    pop ax
+
+    RET
+
+;ax: LBA address
+;cl: number of sectors to read
+;dl: drive number
+;es:bx: memory address where to store read data
+disk_read:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx
+    call lba_to_chs
+    pop ax
+    
+    mov ah, 02h
+    mov di, 3 ;retry count
+
+.retry:
+    pusha
+    stc                 ;some bios don't set carry
+    int 13h
+    jnc .done
+    
+    popa 
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    jmp floppy_error
+
+.done:
+    popa
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+
+    ret
+
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa 
+    ret
+
+
+;print
 
 print:
     PUSH si
@@ -63,6 +159,6 @@ done_print:
     RET
 
 os_boot_msg: DB 'Our OS has booted!', 0x0D, 0x0A, 0
-
+read_failure: DB 'Failed to read floppy', 0x0D, 0x0A, 0
 TIMES 510-($-$$) DB 0
 DW 0AA55h
